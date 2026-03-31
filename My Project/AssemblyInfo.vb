7c1ad6fc52279261f6f64c724205183cd858c776
aigerim2006 <aigerimhalbekova@gmail.com>
2026-03-31 12:22:17 +0600

    Initial commit: добавление файлов проекта и .gitignore

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("lab8")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("lab8")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("1232718a-1809-4b00-9594-349ee9a785f7")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
